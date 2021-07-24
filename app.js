d5b21a358e369595b2b1e4de8faee4b1d44e6eeb
Aleksey Belchenkov <u608110@gmail.com>
2021-07-24 13:43:32 +0400

    Connect Server to Socket

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const http = require('http');
 
 const app = express();
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 
+// middleware
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
     return res.sendFile(__dirname + "/public/index.html");
 });
 
-const PORT = process.env.PORT || 3000;
+io.on('connection', socket => {
+    console.log('User connected to socket.IO server: ' + socket.id);
+});
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 })
